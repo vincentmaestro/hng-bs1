c39c346ebd3c99a39ebfc66160aa5d72ce1b4792
vincentmaestro <maestroobika@gmail.com>
2024-07-01 03:52:57 +0100

    initial commit

---
+++ api/index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+
+const app = express();
+const apiKey = 'JdX78tZKXgvyGqYA3GiAI5TzcZinnZ6z';
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`app started on port ${port}`));
+
+app.get('/', (req, res) => res.send('From backend track HNG Stage 1: HI!'));
+
+app.get('/api/hello', async (req, res) => {
+    const ip = req.headers['x-real-ip'] || req.headers['x-forwarded-for'];
+
+    // const city = await fetch(`http://dataservice.accuweather.com/locations/v1/cities/ipaddress?apikey=${apiKey}&q=${ip}`);
+    // const cityKey = city.Key;
+    // const cityName = city.EnglishName;
+
+    // const weather = await fetch(`http://dataservice.accuweather.com/currentconditions/v1/${cityKey}?apikey=${apiKey}`);
+    // const temperature = weather[0].Temperature;
+    // const temperatureInDegrees = temperature.Metric.Value;
+
+    // const visitor = {
+    //     client_ip: ip,
+    //     location: cityName,
+    //     greeting: `Hello ${req.query.visitor_name}!, the temperature is ${temperatureInDegrees} degrees Celcius in ${cityName}`
+    // }
+
+    res.send(ip);
+});
+
+export default app;
