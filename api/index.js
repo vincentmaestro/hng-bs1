62490134a21ecedf4c9f6cd7f3323359cd0a0a3f
vincentmaestro <maestroobika@gmail.com>
2024-07-01 04:07:44 +0100

    updated

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import express from 'express';
 const app = express();
 
 const port = process.env.PORT || 3000;
+const apiKey = 'JdX78tZKXgvyGqYA3GiAI5TzcZinnZ6z';
 
 app.listen(port, () => console.log(`app started on port ${port}`));
 
@@ -10,7 +11,6 @@ app.get('/', (req, res) => res.send('From backend track HNG Stage 1: HI!'));
 
 app.get('/api/hello', async (req, res) => {
     const ip = req.headers['x-real-ip'] || req.headers['x-forwarded-for'];
-    const apiKey = 'JdX78tZKXgvyGqYA3GiAI5TzcZinnZ6z';
 
     const requestCity = await fetch(`http://dataservice.accuweather.com/locations/v1/cities/ipaddress?apikey=${apiKey}&q=${ip}`);
     const city = await requestCity.json();
@@ -18,17 +18,18 @@ app.get('/api/hello', async (req, res) => {
     const cityName = city.EnglishName;
 
 
-    // const weather = await fetch(`http://dataservice.accuweather.com/currentconditions/v1/${cityKey}?apikey=${apiKey}`);
-    // const temperature = weather[0].Temperature;
-    // const temperatureInDegrees = temperature.Metric.Value;
+    const requestWeather = await fetch(`http://dataservice.accuweather.com/currentconditions/v1/${cityKey}?apikey=${apiKey}`);
+    const weather = await requestWeather.json();
+    const temperature = weather[0].Temperature;
+    const temperatureInDegrees = temperature.Metric.Value;
 
-    // const visitor = {
-    //     client_ip: ip,
-    //     location: cityName,
-    //     greeting: `Hello ${req.query.visitor_name}!, the temperature is ${temperatureInDegrees} degrees Celcius in ${cityName}`
-    // }
+    const visitor = {
+        client_ip: ip,
+        location: cityName,
+        greeting: `Hello ${req.query.visitor_name}!, the temperature is ${temperatureInDegrees} degrees Celcius in ${cityName}`
+    }
 
-    res.send(city);
+    res.send(visitor);
 });
 
 export default app;
